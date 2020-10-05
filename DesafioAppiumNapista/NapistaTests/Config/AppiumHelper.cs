09144d8c162bccd63e23231c081ab1d2c9a8f380
Carolina <Carolina@PTECH-NOTE-JG>
2020-10-05 14:30:07 -0300

    Adição de métodos helpers para abstrair métodos  do webdriver.

--- DesafioAppiumNapista/NapistaTests/Config/AppiumHelper.cs
+++ DesafioAppiumNapista/NapistaTests/Config/AppiumHelper.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +10,85 @@ namespace NapistaTests.Config
 {
     public class AppiumHelper
     {
+        public IWebDriver WebDriver;
+        public readonly ConfigurationHelper Configuration;
+        public WebDriverWait Wait;
+
+        public AppiumHelper(ConfigurationHelper configuration)
+        {
+            var driverOption = new AppiumOptions();
+            driverOption.AddAdditionalCapability(MobileCapabilityType.PlatformName, configuration.PlatformName);
+            driverOption.AddAdditionalCapability(MobileCapabilityType.DeviceName, configuration.DeviceName);
+            driverOption.AddAdditionalCapability(MobileCapabilityType.App, configuration.app);
+
+
+            Wait = new WebDriverWait(WebDriver, TimeSpan.FromSeconds(30));
+        }
+
+        public string ObterUrl()
+        {
+            return WebDriver.Url;
+        }
+
+        public void IrParaUrl(string url)
+        {
+            WebDriver.Navigate().GoToUrl(url);
+        }
+
+        public bool ValidarConteudoUrl(string conteudo)
+        {
+            return Wait.Until(ExpectedConditions.UrlContains(conteudo));
+        }
+
+        public void ClicarBotaoPorId(string botaoId)
+        {
+            var botao = Wait.Until(ExpectedConditions.ElementIsVisible(By.Id(botaoId)));
+            botao.Click();
+        }
+
+        public void ClicarPorXPath(string xPath)
+        {
+            var elemento = Wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(xPath)));
+            elemento.Click();
+        }
+
+        public IWebElement ObterElementoPorClasse(string classeCss)
+        {
+            return Wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName(classeCss)));
+        }
+
+        public IWebElement ObterElementoPorXPath(string xPath)
+        {
+            return Wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(xPath)));
+        }
+
+        public void PreencherTextBoxPorId(string idCampo, string valorCampo)
+        {
+            var campo = Wait.Until(ExpectedConditions.ElementIsVisible(By.Id(idCampo)));
+            campo.SendKeys(valorCampo);
+        }
+        private bool ElementoExistente(By by)
+        {
+            try
+            {
+                WebDriver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        public bool ValidarSeElementoExistePorIr(string id)
+        {
+            return ElementoExistente(By.Id(id));
+        }
+
+        public void Dispose()
+        {
+            WebDriver.Quit();
+            WebDriver.Dispose();
+        }
     }
 }
