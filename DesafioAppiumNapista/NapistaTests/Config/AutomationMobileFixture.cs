3d8717817498a1a62da623eeaf244ead01188c38
Carolina <carolinasantanalouzada@gmail.com>
2020-10-05 08:07:20 -0300

    Modificação no gitignore. Adição de arquivo json para pegar strings necessárias ao teste.  Adição e modificação em classes helpers para pegar seções do appSettings.json. Criação de collectionFixture para testes.

---
+++ DesafioAppiumNapista/NapistaTests/Config/AutomationMobileFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NapistaTests.Config
+{
+    [CollectionDefinition(nameof(AutomationMobileFixtureCollection))]
+    public class AutomationMobileFixtureCollection : ICollectionFixture<AutomationMobileFixture> { }
+    public class AutomationMobileFixture
+    {
+        public AppiumHelper BrowserHelper;
+        public readonly ConfigurationHelper Configuration;
+
+        public AutomationMobileFixture()
+        {
+            Configuration = new ConfigurationHelper();
+            BrowserHelper = new AppiumHelper();
+        }
+    }
+}
---
+++ DesafioAppiumNapista/NapistaTests/Config/ConfigurationHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace NapistaTests.Config
+{
+    public class ConfigurationHelper
+    {
+        private readonly IConfiguration _config;
+
+        public ConfigurationHelper()
+        {
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.json")
+                .Build();
+        }
+
+
+        public string WebDrivers => $"{_config.GetSection("WebDrivers").Value}";
+        public string FolderPath => Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()));
+        public string AppiumServer => $"{_config.GetSection("AppiumServer").Value}";
+        public string DeviceName => $"{_config.GetSection("deviceName").Value}";
+        public string PlatformName => $"{_config.GetSection("platformName").Value}";
+        public string app => $"{_config.GetSection("app").Value}";
+
+    }
+}
