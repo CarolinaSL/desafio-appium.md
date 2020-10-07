89d305090b6f3a12c2574f6c34a411787d9c3e76
Carolina <Carolina@PTECH-NOTE-JG>
2020-10-07 18:59:01 -0300

    Alteração para pegar caminho completo da apk no sistema.

--- DesafioAppiumNapista/NapistaTests/Config/ConfigurationHelper.cs
+++ DesafioAppiumNapista/NapistaTests/Config/ConfigurationHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.IO;
 using System.Reflection;
 
@@ -16,11 +17,10 @@ namespace NapistaTests.Config
                 .Build();
         }
 
-        public string FolderPath => Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()));
         public string AppiumServer => $"{_config.GetSection("AppiumServer").Value}";
         public string DeviceName => $"{_config.GetSection("deviceName").Value}";
         public string PlatformName => $"{_config.GetSection("platformName").Value}";
-        public string app => "C:\\Users\\Carolina\\Workspace\\desafio-appium.md\\DesafioAppiumNapista\\NapistaTests\\app-release.apk";
+        public string app => Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, _config.GetSection("app").Value);
         public string avd => $"{_config.GetSection("avd").Value}";
 
     }
